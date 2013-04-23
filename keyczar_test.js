mitro-co/keyczarjs

commit 716157a8ae85b785aa24d9eeacb5d3f0a11b2e51

keyczar: Encode/decode strings to/from UTF-8 before encryption.
Correctly handles unicode strings. Still handles "binary" strings, but may do
slightly unusual things to it.

keyczar.js
@@ -173,6 +173,8 @@ function _makeKeyczar(data) {
             encoder = keyczar.keyczar_util.encodeBase64Url;
         }
 
+        // encode as UTF-8 in case plaintext contains non-ASCII characters
+        plaintext = forge.util.encodeUtf8(plaintext);
         var message = instance.primary.encrypt(plaintext);
         if (encoder !== null) message = encoder(message);
         return message;
@@ -184,7 +186,8 @@ function _makeKeyczar(data) {
         }
 
         if (decoder !== null) message = decoder(message);
-        return instance.primary.decrypt(message);
+        var plaintext = instance.primary.decrypt(message);
+        return forge.util.decodeUtf8(plaintext);
     };
 
     // Returns the JSON serialization of this keyczar instance.

keyczar_test.js
@@ -145,5 +145,19 @@ function testSession() {
     assert.equal(longMessage, keyczar.decryptWithSession(privatekey, ciphertext));
 }
 
+function testStringEncoding() {
+    var unicodeMessage = 'Emoji key: \ud83d\udd11';
+    var key = readKey('symmetric.json');
+    var encrypted = key.encrypt(unicodeMessage);
+    assert.equal(unicodeMessage, key.decrypt(encrypted));
+
+    var binaryMessage = '';
+    for (var i = 0; i < 256; i++) {
+        binaryMessage += String.fromCharCode(i);
+    }
+    encrypted = key.encrypt(binaryMessage);
+    assert.equal(binaryMessage, key.decrypt(encrypted));
+}
+
 test_util.runTests([testKeyczarRsa, testEncryptAllBytes, testSerializeKeys, testMaxLengthData,
-    testMakeExportRsa, testSymmetric, testRaw, testSession]);
+    testMakeExportRsa, testSymmetric, testRaw, testSession, testStringEncoding]);

commit cdcb27736ee5de3df3c8e0893dc7c4705bd57211

keyczar: Handle decrypting binary data that contains invalid UTF-8 data.
Add encryptBinary/decryptBinary that don't do any encoding.

Encoding from a Javascript string to UTF-8 will always work (I think).
Decoding arbitrary data may not. In particular, the key/iv for session
encryption must not be mangled!

Fixes a bug introduced by commit 716157a8ae

keyczar.js
@@ -260,28 +260,31 @@ function _makeKeyczar(data, password) {
         throw new Error('Unsupported key type/purpose: ' + t + '/' + p);
     }
 
-    instance.encrypt = function(plaintext, encoder) {
-        if (!encoder && encoder !== null) {
-            encoder = keyczar.keyczar_util.encodeBase64Url;
-        }
+    // Takes a raw byte string, returns a raw byte string
+    instance.encryptBinary = function(plaintext) {
+        // TODO: assert that plaintext does not contain special characters
+        return instance.primary.encrypt(plaintext);
+    };
 
+    instance.encrypt = function(plaintext) {
         // encode as UTF-8 in case plaintext contains non-ASCII characters
         plaintext = forge.util.encodeUtf8(plaintext);
-        var message = instance.primary.encrypt(plaintext);
-        if (encoder !== null) message = encoder(message);
+        var message = instance.encryptBinary(plaintext);
+        message = keyczar.keyczar_util.encodeBase64Url(message);
         return message;
     };
 
     // only include decryption if supported by this key type
     if (p == PURPOSE_DECRYPT_ENCRYPT) {
-        instance.decrypt = function(message, decoder) {
-            if (!decoder && decoder !== null) {
-                decoder = keyczar.keyczar_util.decodeBase64Url;
-            }
+        instance.decryptBinary = function(message) {
+            return instance.primary.decrypt(message);
+        };
 
-            if (decoder !== null) message = decoder(message);
+        instance.decrypt = function(message) {
+            message = keyczar.keyczar_util.decodeBase64Url(message);
             var plaintext = instance.primary.decrypt(message);
-            return forge.util.decodeUtf8(plaintext);
+            plaintext = forge.util.decodeUtf8(plaintext);
+            return plaintext;
         };
     }
 
@@ -347,7 +350,7 @@ function createSessionCrypter(key, sessionMaterial) {
         } else {
             rawSessionMaterial = keyczar.keyczar_util.decodeBase64Url(sessionMaterial);
         }
-        var decrypted = key.decrypt(rawSessionMaterial, null);
+        var decrypted = key.decryptBinary(rawSessionMaterial);
         var keyBytes = keyczar.keyczar_util._unpackByteStrings(decrypted);
 
         sessionKey = keyczar.keyczar_util._aesFromBytes(keyBytes[0], keyBytes[1]);
@@ -357,7 +360,7 @@ function createSessionCrypter(key, sessionMaterial) {
 
         // encrypt the key
         var packed = sessionKey.pack();
-        rawSessionMaterial = key.encrypt(packed, null);
+        rawSessionMaterial = key.encryptBinary(packed);
     }
 
     var crypter = {
@@ -365,17 +368,12 @@ function createSessionCrypter(key, sessionMaterial) {
         sessionMaterial: keyczar.keyczar_util.encodeBase64Url(rawSessionMaterial)
     };
 
-    crypter.encrypt = function(plaintext, encoder) {
-        var ciphertext = sessionKey.encrypt(plaintext);
-
-        // TODO: Call non-null, non-undefined encoder()
-        if (encoder !== null) ciphertext = keyczar.keyczar_util.encodeBase64Url(ciphertext);
-        return ciphertext;
+    crypter.encryptBinary = function(plaintext) {
+        // TODO: assert that plaintext has no special chars
+        return sessionKey.encrypt(plaintext);
     };
 
-    crypter.decrypt = function(message, decoder) {
-        // TODO: Call non-null, non-undefined decoder()
-        if (decoder !== null) message = keyczar.keyczar_util.decodeBase64Url(message);
+    crypter.decryptBinary = function(message) {
         return sessionKey.decrypt(message);
     };
 
@@ -387,7 +385,8 @@ function createSessionCrypter(key, sessionMaterial) {
 // Convenience wrapper around a SessionCrypter.
 function encryptWithSession(key, message) {
     var crypter = createSessionCrypter(key);
-    var rawEncrypted = crypter.encrypt(message, null);
+    message = forge.util.encodeUtf8(message);
+    var rawEncrypted = crypter.encryptBinary(message);
     var packed = keyczar.keyczar_util._packByteStrings([crypter.rawSessionMaterial, rawEncrypted]);
     return keyczar.keyczar_util.encodeBase64Url(packed);
 }
@@ -396,7 +395,8 @@ function decryptWithSession(key, message) {
     message = keyczar.keyczar_util.decodeBase64Url(message);
     var unpacked = keyczar.keyczar_util._unpackByteStrings(message);
     var crypter = createSessionCrypter(key, unpacked[0]);
-    return crypter.decrypt(unpacked[1], null);
+    var plaintext = crypter.decryptBinary(unpacked[1]);
+    return forge.util.decodeUtf8(plaintext);
 }
 
 keyczar.TYPE_RSA_PRIVATE = TYPE_RSA_PRIVATE;

roundtripper.js
@@ -18,8 +18,8 @@ function encrypt(keyPath, message, outputPath) {
     writeFile(outputPath, encrypted);
 }
 
-function decrypt(keyPath, encryptedPath, expectedMessage, expectedType) {
-    var key = keyczar.fromJson(readFile(keyPath));
+function decrypt(keyPath, encryptedPath, expectedMessage, expectedType, keyPassword) {
+    var key = keyczar.fromJson(readFile(keyPath), keyPassword);
     if (key.metadata.type != expectedType) {
         throw new Error('Unexpected key type: ' + key.metadata.type);
     }
@@ -37,6 +37,10 @@ function decrypt(keyPath, encryptedPath, expectedMessage, expectedType) {
 }
 
 function makeLonger(input) {
+    if (input.length === 0) {
+        input = '\x00';
+    }
+
     while (input.length < 1000) {
         input += input;
     }
@@ -68,6 +72,7 @@ if (process.argv.length != 4 && process.argv.length != 5) {
 var mode = process.argv[2];
 var dirpath = process.argv[3];
 var message = 'Hello this is a longish message from Javascript';
+var password = 'foopassword';
 if (process.argv.length == 5) {
     message = process.argv[4];
 }
@@ -82,6 +87,9 @@ if (mode == 'encrypt') {
 
     console.log('encrypting message length', message.length);
     encrypt(dirpath + '/publickey.json', message, dirpath + '/publickey_encrypted');
+
+    console.log('encrypting key with password');
+    writeFile(dirpath + '/privatekey_encrypted.json', privateKey.toJsonEncrypted(password));
 
     console.log('generating AES key ...');
     var symmetric = keyczar.create(keyczar.TYPE_AES);
@@ -99,6 +107,12 @@ if (mode == 'encrypt') {
 } else if (mode == 'roundtrip') {
     var decrypted = decrypt(dirpath + '/privatekey.json', dirpath + '/publickey_encrypted', null, keyczar.TYPE_RSA_PRIVATE);
     encrypt(dirpath + '/publickey.json', decrypted, dirpath + '/publickey_reencrypted');
+
+    var decrypted2 = decrypt(dirpath + '/privatekey_encrypted.json', dirpath + '/publickey_encrypted', null, keyczar.TYPE_RSA_PRIVATE, password);
+    if (decrypted2 != decrypted) {
+        process.stderr.write('encrypted private key did not work?\n');
+        process.exit(1);
+    }
 
     decrypted = decrypt(dirpath + '/symmetric.json', dirpath + '/symmetric_encrypted', null, keyczar.TYPE_AES);
     encrypt(dirpath + '/symmetric.json', decrypted, dirpath + '/symmetric_reencrypted');

keyczar_test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var fs = require('fs');
 
 var keyczar = require('./keyczar');
+var keyczar_util = require('./keyczar_util');
 var test_util = require('./test_util');
 
 function readTestData(name) {
@@ -119,32 +120,45 @@ function testSymmetric() {
     assert.equal(EXAMPLE_MESSAGE, key.decrypt(encrypted));
 }
 
-function testRaw() {
+function testRawBinary() {
     var key = readKey('symmetric.json');
     // not base64 encoded
-    var encrypted = key.encrypt(EXAMPLE_MESSAGE, null);
+    var encrypted = key.encryptBinary(EXAMPLE_MESSAGE);
     assert.equal(0x00, encrypted.charCodeAt(0));
-    var decrypted = key.decrypt(encrypted, null);
+    var decrypted = key.decryptBinary(encrypted);
     assert.equal(EXAMPLE_MESSAGE, decrypted);
+
+    // it must be possible to encrypt/decrypt this invalid utf-8 string: it
+    // can be generated by Keyczar's session encryption
+    key = readKey('symmetric_binary.json');
+    encrypted = keyczar_util.decodeBase64Url(readTestData('symmetric_binary_encrypted'));
+    decrypted = key.decryptBinary(encrypted);
+    // '\x96' cannot be decoded from UTF-8: it is an invalid UTF-8 sequence
+    assert.equal('\x96', decrypted.charAt(decrypted.length-1));
 }
 
 function testSession() {
     var publickey = readKey('publickey.json');
     var session = keyczar.createSessionCrypter(publickey);
-    var ciphertext = session.encrypt(EXAMPLE_MESSAGE);
+    var ciphertext = session.encryptBinary(EXAMPLE_MESSAGE);
     var material = session.sessionMaterial;
 
     var privatekey = readKey('privatekey.json');
     session = keyczar.createSessionCrypter(privatekey, material);
-    assert.equal(EXAMPLE_MESSAGE, session.decrypt(ciphertext));
+    assert.equal(EXAMPLE_MESSAGE, session.decryptBinary(ciphertext));
 
     var longMessage = 'long message ';
-    for (var i = 0; i < 6; i++) {
+    while (longMessage.length < 500) {
         longMessage += longMessage;
     }
-    assert(longMessage.length > 500);
-    ciphertext = keyczar.encryptWithSession(publickey, longMessage);
-    assert.equal(longMessage, keyczar.decryptWithSession(privatekey, ciphertext));
+
+    // repeat this multiple times to ensure random binary data passes through okay
+    // this caught the invalid UTF-8 decoding bug
+    for (var j = 0; j < 5; j++) {
+        ciphertext = keyczar.encryptWithSession(publickey, longMessage);
+        assert.equal(longMessage, keyczar.decryptWithSession(privatekey, ciphertext));
+        process.stdout.write('!');
+    }
 }
 
 function testStringEncoding() {
@@ -152,6 +166,11 @@ function testStringEncoding() {
     var key = readKey('symmetric.json');
     var encrypted = key.encrypt(unicodeMessage);
     assert.equal(unicodeMessage, key.decrypt(encrypted));
+
+    // pass the unicode data through a session
+    var privateKey = readKey('privatekey.json');
+    encrypted = keyczar.encryptWithSession(privateKey, unicodeMessage);
+    assert.equal(unicodeMessage, keyczar.decryptWithSession(privateKey, encrypted));
 
     var binaryMessage = '';
     for (var i = 0; i < 256; i++) {
@@ -205,4 +224,5 @@ function testEncryptedKey() {
 }
 
 test_util.runTests([testKeyczarRsa, testEncryptAllBytes, testSerializeKeys, testMaxLengthData,
-    testMakeExportRsa, testSymmetric, testRaw, testSession, testStringEncoding, testEncryptedKey]);
+    testMakeExportRsa, testSymmetric, testRawBinary, testSession, testStringEncoding,
+    testEncryptedKey]);

testdata/symmetric_binary_encrypted
@@ -1,0 +1,1 @@
+ANes_qOT85sGKMBtt5reu3_oaIfMhpng910ngz0CNWokLCr6SPItfDqrrCWBWgtosAxW5f6_9VKkU3xZh0vsSdqm5iL5-rJom-Jebs061YjVBvC21cLQJLA